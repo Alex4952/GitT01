8274f59a3858544f0c9e704ac5147ce30ce8d56a
Alex4952 <keyzoo@gmail.com>
2020-07-10 08:31:19 +0900

    Add add()

--- Calc01/Calc01/main.cpp
+++ Calc01/Calc01/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #pragma warning (disable : 4996 6031)
 using namespace std;
+
+int add(int x, int y) {
+	return x + y;
+}
+
 int main() {
 	cout << "original_source" << endl;
 	cout << "to test sync" << endl;
 	cout << "to test push" << endl;
 
+	cout << add(10, 3) << endl;
 	return 0;
 }
